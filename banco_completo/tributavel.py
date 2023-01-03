5463dcb950cf8b03deb3e1fe7ab014e58a0d5ad1
luizznelson <111987288+luizznelson@users.noreply.github.com>
2023-01-03 10:36:38 -0300

    Delete banco_completo directory

--- banco_completo/tributavel.py
+++
@@ -1,8 +0,0 @@
-import abc
-
-class Tributavel(abc.ABC):
-
-    @abc.abstractmethod
-    def get_valor_tributado(self):
-        ''' Esse metodo devera retornar o valor a ser tributado da classe'''
-        pass
